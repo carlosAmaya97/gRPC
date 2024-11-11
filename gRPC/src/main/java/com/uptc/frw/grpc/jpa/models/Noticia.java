56576b7249dd5f283fc605461a5db62e4d3a43ba
CristianFCantor <142556790+CristianFCantor@users.noreply.github.com>
2024-11-11 18:56:05 -0500

    Update Noticia.java

--- gRPC/src/main/java/com/uptc/frw/grpc/jpa/models/Noticia.java
+++ gRPC/src/main/java/com/uptc/frw/grpc/jpa/models/Noticia.java
@@ -1,6 +1,7 @@
 package com.uptc.frw.grpc.jpa.models;
 import jakarta.persistence.*;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -23,16 +24,117 @@ public class Noticia {
     @Column(name = "TEXTO")
     private String texto;
 
-    // Relación con PeriodistaNoticia (muchos a muchos a través de tabla intermedia)
-    @OneToMany(mappedBy = "noticia")
-    private Set<PeriodistaNoticia> periodistas;
+    // Relación con Periodista (muchos a muchos a través de tabla intermedia)
+    @ManyToMany(mappedBy = "noticia")
+    private List<Periodista> periodistas;
 
-    // Relación con NoticiaImplicado (muchos a muchos a través de tabla intermedia)
-    @OneToMany(mappedBy = "noticia")
-    private Set<NoticiaImplicado> implicados;
+    // Relación con Implicado (muchos a muchos a través de tabla intermedia)
+    @ManyToMany(mappedBy = "noticia")
+    private List<Implicado> implicados;
 
-    // Relación con NoticiaAgencia (muchos a muchos a través de tabla intermedia)
-    @OneToMany(mappedBy = "noticia")
-    private Set<NoticiaAgencia> agencias;
+    // Relación con Agencia (muchos a muchos a través de tabla intermedia)
+    @ManyToMany(mappedBy = "noticia")
+    private List<Agencia> agencias;
 
+    // Relacion consigo misma por ID_R
+    @ManyToOne
+    @JoinColumn(name = "ID_R")
+    private Noticia noticiaRelacionada;
+
+    @OneToMany(mappedBy = "noticiaRelacionada")
+    private List<Noticia> noticiasRelacionadas;
+
+    public Noticia() {
+    }
+
+    public long getIdN() {
+        return idN;
+    }
+
+    public void setIdN(long idN) {
+        this.idN = idN;
+    }
+
+    public long getIdR() {
+        return idR;
+    }
+
+    public void setIdR(long idR) {
+        this.idR = idR;
+    }
+
+    public Date getFechaN() {
+        return fechaN;
+    }
+
+    public void setFechaN(Date fechaN) {
+        this.fechaN = fechaN;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public List<Periodista> getPeriodistas() {
+        return periodistas;
+    }
+
+    public void setPeriodistas(List<Periodista> periodistas) {
+        this.periodistas = periodistas;
+    }
+
+    public List<Implicado> getImplicados() {
+        return implicados;
+    }
+
+    public void setImplicados(List<Implicado> implicados) {
+        this.implicados = implicados;
+    }
+
+    public List<Agencia> getAgencias() {
+        return agencias;
+    }
+
+    public void setAgencias(List<Agencia> agencias) {
+        this.agencias = agencias;
+    }
+
+    public Noticia getNoticiaRelacionada() {
+        return noticiaRelacionada;
+    }
+
+    public void setNoticiaRelacionada(Noticia noticiaRelacionada) {
+        this.noticiaRelacionada = noticiaRelacionada;
+    }
+
+    public List<Noticia> getNoticiasRelacionadas() {
+        return noticiasRelacionadas;
+    }
+
+    public void setNoticiasRelacionadas(List<Noticia> noticiasRelacionadas) {
+        this.noticiasRelacionadas = noticiasRelacionadas;
+    }
+
+    @Override
+    public String toString() {
+        return "Noticia{" +
+                "idN=" + idN +
+                ", idR=" + idR +
+                ", fechaN=" + fechaN +
+                ", titular='" + titular + '\'' +
+                ", texto='" + texto + '\'' +
+                '}';
+    }
 }
