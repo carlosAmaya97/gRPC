1dc5a5b87fd5b9247e7f1ba4ce96705801c0268f
CristianFCantor <142556790+CristianFCantor@users.noreply.github.com>
2024-11-11 18:57:01 -0500

    Update Implicado.java

--- gRPC/src/main/java/com/uptc/frw/grpc/jpa/models/Implicado.java
+++ gRPC/src/main/java/com/uptc/frw/grpc/jpa/models/Implicado.java
@@ -1,5 +1,7 @@
+package com.uptc.frw.grpc.jpa.models;
 import jakarta.persistence.*;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -19,13 +21,83 @@ public class Implicado {
     @Column(name = "CALIDAD")
     private String calidad;
 
-    // Relación con NoticiaImplicado (muchos a muchos a través de tabla intermedia)
-    @OneToMany(mappedBy = "implicado")
-    private Set<NoticiaImplicado> noticias;
+    // Relación con Noticias (muchos a muchos a través de tabla intermedia)
+    @ManyToMany
+    @JoinTable(
+            name = "NOTICIA_IMPLICADO",
+            joinColumns = @JoinColumn(name = "ID_I"),
+            inverseJoinColumns = @JoinColumn(name = "ID_N")
+    )
+    private List<Noticia> noticias;
 
-    // Relación con PeriodistaImplicados (muchos a muchos a través de tabla intermedia)
-    @OneToMany(mappedBy = "implicado")
-    private Set<PeriodistaImplicados> periodistas;
+    // Relación con Periodistas(muchos a muchos a través de tabla intermedia)
+    @ManyToMany
+    @JoinTable(
+            name = "PERIODISTA_IMPLICADOS",
+            joinColumns = @JoinColumn(name = "ID_I"),
+            inverseJoinColumns = @JoinColumn(name = "ID_P")
+    )
+    private List<Periodista> periodistas;
 
+    public Implicado() {
+    }
+
+    public long getIdI() {
+        return idI;
+    }
+
+    public void setIdI(long idI) {
+        this.idI = idI;
+    }
+
+    public String getNombreI() {
+        return nombreI;
+    }
+
+    public void setNombreI(String nombreI) {
+        this.nombreI = nombreI;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getCalidad() {
+        return calidad;
+    }
+
+    public void setCalidad(String calidad) {
+        this.calidad = calidad;
+    }
+
+    public List<Noticia> getNoticias() {
+        return noticias;
+    }
+
+    public void setNoticias(List<Noticia> noticias) {
+        this.noticias = noticias;
+    }
+
+    public List<Periodista> getPeriodistas() {
+        return periodistas;
+    }
+
+    public void setPeriodistas(List<Periodista> periodistas) {
+        this.periodistas = periodistas;
+    }
+
+    @Override
+    public String toString() {
+        return "Implicado{" +
+                "calidad='" + calidad + '\'' +
+                ", fechaNacimiento=" + fechaNacimiento +
+                ", nombreI='" + nombreI + '\'' +
+                ", idI=" + idI +
+                '}';
+    }
 }
 
